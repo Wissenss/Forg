9e0afe42df7e4e5c8492ec77ebe2a644d47896a0
merinogale@gmail.com <merinogale@gmail.com>
2025-07-19 07:44:20 -0600

    removed unused models

--- models/account.py
+++
@@ -1,8 +0,0 @@
-import constants
-
-class Account():
-    def __init__(self):
-        self.id : int = 0
-        self.discord_user_id : int = 0
-        self.discord_guild_id : int = 0
-        self.access_level : constants.AccountAccessLevel = constants.AccountAccessLevel.MEMBER
